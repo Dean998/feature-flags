eacb32946d8930b089d2993cfca15351de73055d
dean998 <dean.b.98@hotmail.com>
2022-05-30 14:04:55 +0100

    created function with all criteria that needs to be fufilled

--- src/index.js
+++ src/index.js
@@ -31,29 +31,39 @@ let user = {
 
 //Creating a function to see if criteria is fufilled
 const checker= () => {
-    //Creating a nested for loop to check if within a feature which users are supported
-
+//Creating a nested for loop to check if within a feature which users are supported
     for(let i = 0 ; i < features.length ; i++){
-  
     //within each feature we then loop through all the different users to see if they are elegible  
-    // console.log(users[i])
-    for(let j = 0 ; j < exampleUsers.length; j++){
-      //if email is included then these statements will run
-      if(features[i].enabledEmails.includes(exampleUsers[j].email)){
-        //running through each feature to see which users are enabled for each user and pushing to each respective feature
-        //Super Cool Feature 
-        if(features[i].name == "SuperCoolFeature")
-        {superCoolFeature.push(exampleUsers[j])}
-        //SimplifiedNavBar
-        if(features[i].name == "SimplifiedNavBar")
-        {simplifiedNavBar.push(exampleUsers[j])}
-        //EnhancedDashboardFeature
-        if(features[i].name == "EnhancedDashboardFeature")
-        {enhancedDashboardFeature.push(exampleUsers[j])}
-        //newUserOnBoardingJourney
-        if(features[i].name == "NewUserOnboardingJourney")
-        {newUserOnBoardingJourney.push(exampleUsers[j])}
-      } 
+        for(let j = 0 ; j < exampleUsers.length; j++){
+            //if email is included then these statements will run
+            if(features[i].enabledEmails.includes(exampleUsers[j].email)){
+            //running through each feature to see which users are enabled for each user and pushing to each respective feature
+                //Super Cool Feature 
+                if(features[i].name == "SuperCoolFeature")
+                    {superCoolFeature.push(exampleUsers[j])}
+                //SimplifiedNavBar
+                if(features[i].name == "SimplifiedNavBar")
+                    {simplifiedNavBar.push(exampleUsers[j])}
+                //EnhancedDashboardFeature
+                if(features[i].name == "EnhancedDashboardFeature")
+                    {enhancedDashboardFeature.push(exampleUsers[j])}
+                //newUserOnBoardingJourney
+                if(features[i].name == "NewUserOnboardingJourney")
+                    {newUserOnBoardingJourney.push(exampleUsers[j])}
+            } 
+            //extra section for enhanced dashboard and newuseronboarding based on odds and location
+
+    if(features[i].name == 'EnhancedDashboardFeature' && Math.random() > 0.5 && features[i].includedCountries.includes(exampleUsers[j].location))
+    {enhancedDashboardFeature.push(exampleUsers[j]) }
+
+    //newuseronboarding based on odds and location
+    if(features[i].name == 'NewUserOnboardingJourney' && Math.random() < 0.25 && !features[i].excludedCountries.includes(exampleUsers[j].location) && exampleUsers[j].email.includes(newUserOnBoardingJourney) )
+    {newUserOnBoardingJourney.push(exampleUsers[j]) }
+
+    //Assuming marketing banner is the only one that doesnt matter in terms of ratio as only those in the country can use it plus ratio is 1 so guarenteed for US citizens to see it 
+    //Marketing Banner
+    if(exampleUsers[j].location.includes(features[i].includedCountries) && features[i].name == "MarketingBanner")
+      {marketingBanner.push(exampleUsers[j])}
         }
     }
     console.log("SuperCoolFeature" , superCoolFeature)
@@ -82,6 +92,11 @@ app.get('/users' , (req,res) => {
 //gets the page when the server runs and displays the features
 app.get('/features' , (req,res) => {
     res.send(features)
+})
+
+//displaying the emails of users supported in a certain feature
+app.get('/supported' , (req,res) => {
+    res.send(`<h1>${enhancedDashboardFeature.map(feature => feature.email)}</h1>`)
 })
 
 
